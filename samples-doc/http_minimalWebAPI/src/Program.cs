286738173313595be8bc8770682887bf9a98a9d4
Harald Sack <harald.sack@t-systems.com>
2025-12-18 14:31:36 +0100

    http_minimalWebApi added: swagger-ui, logging

--- samples-doc/http_minimalWebAPI/src/Program.cs
+++ samples-doc/http_minimalWebAPI/src/Program.cs
@@ -1,40 +1,109 @@
 namespace http_minimalWebAPI;
 
+using System.Linq.Expressions;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Context;
+using Serilog.Core;
+using Serilog.Expressions;
+using Serilog.Extensions.Hosting;
+using Serilog.Formatting.Compact;
+using Serilog.Formatting.Json;
+using Serilog.Templates;
+
 public class Program
 {
+
+
   public static void Main(string[] args)
   {
-    var builder = WebApplication.CreateBuilder(); 
+
+    // Create the WebApplication builder
+    var builder = WebApplication.CreateBuilder();
+
+    // timezone conversion functions for Serilog.Expressions
+    var dateTimeFunctions = new StaticMemberNameResolver(typeof(DateTimeFunctions));
+
+    // Configure Serilog
+    builder.Host.UseSerilog((context, loggerConfig) =>
+        loggerConfig
+        .WriteTo.Async(a => a.Console(
+          new ExpressionTemplate(
+            "{ { ts: ToUtc(@t), requestId: XCFFRequestId, lvl: @l,  msg: @m, threadId: ThreadId } }\n",
+            nameResolver: dateTimeFunctions
+          )
+        ), bufferSize: 50)
+        .Enrich.FromLogContext()
+        .Enrich.WithThreadId()
+    );
+
+
+    // add swagger
+    builder.Services.AddControllers();
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options =>
+    {
+      options.SwaggerDoc("v1", new OpenApiInfo { Title = "Minimal Web API", Version = "v1" });
+
+    });
 
     var app = builder.Build();
+
+    // Use the custom logging middleware
+    app.UseMiddleware<LoggingMiddleware>();
 
     // Enable serving static files (including favicon.ico from wwwroot)
     app.UseStaticFiles();
 
-    // Minimal API endpoint definitions
-    app.MapGet("/", () => "Hello World!");
+    // Enable Swagger middleware
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
-    // Route parameter example: /greeting/John
-    app.MapGet("/greeting/{name}", (string name) =>
-    {
-      return $"Hello, {name}!";
-    });
-
-
-    // Query parameter example: /hello?name=John
-    app.MapGet("/hello", (string? name) =>
-    {
-      return $"Hello, {name ?? "Guest"}!";
-    });
-
-    app.MapGet("/test", async context =>
-    {
-      string? requestId = context.Request.Headers["x-cff-request-id"];
-
-      await context.Response.WriteAsync($"Request ID is: {requestId}");
-    });
+    // Map API endpoints
+    APIEndpoints.Map(app);
 
     // Start the application
     app.Run();
+  }
+
+  public static class APIEndpoints
+  {
+    public static void Map(WebApplication app)
+    {
+      // Minimal API endpoint definitions
+      app.MapGet("/", () => "Hello World!");
+
+
+      // Route parameter example: /greeting/John
+      app.MapGet("/greeting/{name}", (string name) =>
+      {
+        Log.Information("Greeting {Name}", name);
+        return $"Hello, {name}!";
+      });
+
+
+      // Query parameter example: /hello?name=John
+      app.MapGet("/hello", (string? name) =>
+      {
+        return $"Hello, {name ?? "Guest"}!";
+      });
+
+      app.MapGet("/test", async context =>
+      {
+        string? requestId = context.Request.Headers["x-cff-request-id"];
+
+        await context.Response.WriteAsync($"Request ID is: {requestId}");
+      });
+    }
   }
 }
